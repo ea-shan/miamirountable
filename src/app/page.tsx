2574d3f46e9fa961645e7a356de11a636c5d8cdf
ea-shan <shantanu.goswami@expressanalytics.net>
2026-09-11 19:26:07 +0530

    First updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,6 +38,7 @@ export default function Home() {
       <NewsletterSection />
       <SectionLine />
       <FaqSection />
+      <SectionLine />
       <SiteFooter />
       <CookieBanner />
     </main>
